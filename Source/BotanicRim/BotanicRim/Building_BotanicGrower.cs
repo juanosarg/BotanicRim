4fb95ba184f776dbea06c578403ef9311d096506
juanosarg <31105173+juanosarg@users.noreply.github.com>
2019-05-12 14:47:50 +0200

    Initial commit

---
+++ Source/BotanicRim/BotanicRim/Building_BotanicGrower.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Verse;
+using RimWorld;
+
+namespace BotanicRim
+{
+    public class Building_BotanicGrower : Building_PlantGrower
+    {
+        
+
+        public override string GetInspectString()
+        {
+            string text = "Oh my god \n";
+            if (base.Spawned)
+            {
+                
+                if (PlantUtility.GrowthSeasonNow(base.Position, base.Map, true))
+                {
+                    text = text + "\n" + "GrowSeasonHereNow".Translate();
+                }
+                else
+                {
+                    text = text + "\n" + "CannotGrowBadSeasonTemperature".Translate();
+                }
+            }
+            return text;
+        }
+
+     
+    }
+}
+
