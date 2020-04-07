ab692445f4bdda92c0bb1f7152ecc71960fc2153
juanosarg <31105173+juanosarg@users.noreply.github.com>
2020-04-07 21:05:24 +0200

    v0.1

---
+++ 1.1/Source/BotanicRim/BotanicRim/HarmonyPatchers.cs
@@ -0,0 +1,65 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Reflection;
+using Verse;
+
+using System;
+
+// So, let's comment this code, since it uses Harmony and has moderate complexity
+
+namespace BotanicRim
+{
+
+
+
+    //Setting the Harmony instance
+    [StaticConstructorOnStartup]
+    public class Main
+    {
+        static Main()
+        {
+            var harmony = new Harmony("com.botanicrim");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+
+    }
+
+
+
+
+
+    /*This Harmony Postfix allows us to regrow plants with a 25% percentage if an active beehouse is nearby
+*/
+    [HarmonyPatch(typeof(PlantUtility))]
+    [HarmonyPatch("CanSowOnGrower")]
+    public static class PlantUtility_CanSowOnGrower_Patch
+    {
+        [HarmonyPostfix]
+        public static void SowTagsOnBotanicPlants( ThingDef plantDef, object obj, ref bool __result)
+        {
+            if (obj is Zone_GrowingBotanics)
+            {
+                __result = plantDef.plant.sowTags.Contains("BR_Zone_Planting");
+            }
+
+
+
+        }
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
